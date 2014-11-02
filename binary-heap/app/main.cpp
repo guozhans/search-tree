81953a02cf32a3a081cd87683d6a8817217e67a8
guozhans <guozhan.s@gmail.com>
2014-11-03 00:11:22 +0800

    Add DeleteMin method, and test code.

--- binary-heap/app/main.cpp
+++ binary-heap/app/main.cpp
@@ -2,6 +2,21 @@
 
 int main()
 {
+  int node = 0;
   CBinaryHeap<int> heap;
+  heap.Insert(7);
+  heap.Insert(4);
+  heap.Insert(6);
+  heap.Insert(3);
+  heap.Insert(5);
+  heap.Insert(1);
+  heap.Insert(2);
+  heap.DumpHeap();
+  for (int i = 0; i < 8; i++)
+  {
+    heap.DeleteMin(node);
+    cout << "Mini node: " << node << "\n" ;
+    heap.DumpHeap();
+  }
   return 0;
 }
--- binary-heap/src/BinaryHeap.h
+++ binary-heap/src/BinaryHeap.h
@@ -2,10 +2,12 @@
 #define BINARY_HEAP_H
 
 #include <vector>
+#include <iostream>
+#include <assert.h>
 
 using namespace std;
 
-static const int KDefaultIncreaseSize = 128;
+static const int KDefaultIncreaseSize = 4;
 
 template <typename T>
 class CBinaryHeap
@@ -17,9 +19,15 @@ public:
   
   //The methods of binary heap
   void Insert(const T& aNode);
-
+  void DeleteMin(T& aMinNode);
+  void DumpHeap();
+  
 private:
   int GetParentIndex(const int aChildIndex) const;
+  int GetLeftNodeIndex(const int aParentIndex) const;
+  int GetRightNodeIndex(const int aParentIndex) const;
+  void MoveUp(int aIndex, const T& aNode);
+  void MoveDown(int aIndex, const T& aNode);
   
 private:
   int iSize;
@@ -27,25 +35,21 @@ private:
 };
 
 
-template <typename T>
-CBinaryHeap<T>::CBinaryHeap()
+template <typename T> CBinaryHeap<T>::CBinaryHeap()
   : iSize(0)
 {}
 
-template <typename T> 
-CBinaryHeap<T>::CBinaryHeap(const vector<T>& aHeap)
+template <typename T> CBinaryHeap<T>::CBinaryHeap(const vector<T>& aHeap)
 {
 }
 
-template <typename T>
-CBinaryHeap<T>::~CBinaryHeap()
+template <typename T> CBinaryHeap<T>::~CBinaryHeap()
 {
   iContainer.clear();
   iSize = 0;
 }
 
-template <typename T>
-int CBinaryHeap<T>::GetParentIndex(const int aChildIndex) const
+template <typename T> int CBinaryHeap<T>::GetParentIndex(const int aChildIndex) const
 {
   if (aChildIndex > 0)
   {
@@ -57,18 +61,21 @@ int CBinaryHeap<T>::GetParentIndex(const int aChildIndex) const
   }
 }
 
-template <typename T>
-void CBinaryHeap<T>::Insert(const T& aNode)
+template <typename T> int CBinaryHeap<T>::GetLeftNodeIndex(const int aParentIndex) const
 {
-  if (iSize == iContainer.size())
+  return aParentIndex * 2 + 1;
+}
+
+template <typename T> int CBinaryHeap<T>::GetRightNodeIndex(const int aParentIndex) const
+{
+  return aParentIndex * 2 + 2;
+}
+
+template <typename T> void CBinaryHeap<T>::MoveUp(int aIndex, const T& aNode)
+{
+  for (int holeIndex = aIndex; holeIndex >= 0; holeIndex = GetParentIndex(holeIndex))
   {
-    iContainer.resize(iContainer.size() + KDefaultIncreaseSize);
-  }
-  
-  int holeIndex = iSize++;
-  for (; holeIndex >= 0; holeIndex = GetParentIndex(holeIndex))
-  {
-    if (iContainer[GetParentIndex(holeIndex)] > aNode)
+    if (iContainer[GetParentIndex(holeIndex)] > aNode && holeIndex > 0)
     {
       iContainer[holeIndex] = iContainer[GetParentIndex(holeIndex)];
     }
@@ -79,5 +86,65 @@ void CBinaryHeap<T>::Insert(const T& aNode)
     }
   }
 }
+
+template <typename T> void CBinaryHeap<T>::MoveDown(int aIndex, const T& aNode)
+{
+  int holeIndex = aIndex;
+  int child = 0;
+  for (; (holeIndex * 2) < (iSize - 1); holeIndex = child)
+  {
+    int rightNodeIndex = GetRightNodeIndex(holeIndex);
+	child = GetLeftNodeIndex(holeIndex);
+    if ((child < (iSize - 1)) && (iContainer[child] > iContainer[rightNodeIndex]))
+    {
+      child = GetRightNodeIndex(holeIndex);
+    }
+    
+    if (aNode > iContainer[child])
+    {
+      iContainer[holeIndex] = iContainer[child];
+    }
+    else
+    {
+      break;
+    }
+  }
+  
+  iContainer[holeIndex] = aNode;
+}
+
+template <typename T> void CBinaryHeap<T>::Insert(const T& aNode)
+{
+  if (iSize == iContainer.size())
+  {
+    iContainer.resize(iContainer.size() + KDefaultIncreaseSize);
+  }
+  
+  int holeIndex = iSize++;
+  MoveUp(holeIndex, aNode);
+}
+
+template <typename T> void CBinaryHeap<T>::DeleteMin(T& aMinNode)
+{
+  if (iContainer.size() == 0 || iSize == 0)
+  {
+    return;
+  }
+  aMinNode = iContainer[0];
+  MoveDown(0, iContainer[--iSize]);
+}
+
+template <typename T> void CBinaryHeap<T>::DumpHeap()
+{
+  int oount = 0;
+  vector<T>::iterator it = iContainer.begin();
+  cout << "===> Dump heap: ";
+  while (it != iContainer.end() && oount++ < iSize)
+  {
+    cout << *it << " | ";
+	++it;
+  }
+}
+
 
 #endif //BINARY_HEAP_H
